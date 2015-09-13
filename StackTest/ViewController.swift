bc97694072a5b5bf65c2ee478808c86a91225f66
Umberto Raimondi <me@uraimo.com>
2015-09-13 11:51:53 +0200

    First part of the tutorial, simple example

--- StackTest/ViewController.swift
+++ StackTest/ViewController.swift
@@ -9,10 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var stackView:UIStackView!
+    var lastLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints=false
+        self.view.addSubview(stackView)
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-30-[stackView]-30-|",
+            options: NSLayoutFormatOptions.AlignAllLeading,metrics: nil, views: ["stackView":stackView]))
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-10-[stackView]-10-|",
+            options: NSLayoutFormatOptions.AlignAllLeading,metrics: nil, views: ["stackView":stackView]))
+        
+        
+        stackView.axis = .Vertical
+        stackView.alignment = .Fill
+        stackView.spacing = 25
+        stackView.distribution = .FillProportionally
+        
+        var lbl = UILabel()
+        lbl.text="Label 1"
+        lbl.backgroundColor = UIColor.redColor()
+        stackView.addArrangedSubview(lbl)
+        
+        lbl = UILabel()
+        lbl.text="Label 2"
+        lbl.backgroundColor = UIColor.greenColor()
+        stackView.addArrangedSubview(lbl)
+        
+        lastLabel.text="Label 3"
+        lastLabel.backgroundColor = UIColor.blueColor()
+        stackView.addArrangedSubview(lastLabel)
+        
+        let btn=UIButton(type: .System)
+        btn.setTitle("Press Me", forState: .Normal)
+        btn.addTarget(self, action: "pressedme:", forControlEvents: UIControlEvents.TouchUpInside)
+        stackView.addArrangedSubview(btn)
+        
+    }
+    
+    func pressedme(sender: UIButton!){
+        UIView.animateWithDuration(0.5) {
+            self.lastLabel.hidden = !self.lastLabel.hidden
+        }
+        //stackView.removeArrangedSubview(lastLabel)
+        //lastLabel.removeFromSuperview()
     }
 
     override func didReceiveMemoryWarning() {
