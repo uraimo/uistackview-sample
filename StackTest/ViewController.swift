f87087fe0da1a37195e45b18cd39583159e2b16c
Umberto Raimondi <me@uraimo.com>
2015-09-14 15:31:15 +0200

    Extended sample with new constraints and an inner UIStackView

--- StackTest/ViewController.swift
+++ StackTest/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     var stackView:UIStackView!
-    var lastLabel = UILabel()
+    var nestedStackView = UIStackView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
         stackView = UIStackView()
         stackView.translatesAutoresizingMaskIntoConstraints=false
         self.view.addSubview(stackView)
+        // Main UIStackView contraints, nearly fills its parent view
         self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-30-[stackView]-30-|",
             options: NSLayoutFormatOptions.AlignAllLeading,metrics: nil, views: ["stackView":stackView]))
         self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-10-[stackView]-10-|",
@@ -28,23 +29,32 @@ class ViewController: UIViewController {
         stackView.axis = .Vertical
         stackView.alignment = .Fill
         stackView.spacing = 25
-        stackView.distribution = .FillProportionally
+        stackView.distribution = .FillEqually
         
         var lbl = UILabel()
         lbl.text="Label 1"
         lbl.backgroundColor = UIColor.redColor()
+        lbl.setContentHuggingPriority(1000, forAxis: UILayoutConstraintAxis.Vertical)
+        
         stackView.addArrangedSubview(lbl)
         
         lbl = UILabel()
         lbl.text="Label 2"
         lbl.backgroundColor = UIColor.greenColor()
+        lbl.setContentHuggingPriority(900, forAxis: UILayoutConstraintAxis.Vertical)
         stackView.addArrangedSubview(lbl)
         
-        lastLabel.text="Label 3"
-        lastLabel.backgroundColor = UIColor.blueColor()
-        stackView.addArrangedSubview(lastLabel)
+        nestedStackView.axis = .Horizontal
+        nestedStackView.alignment = .Fill
+        nestedStackView.spacing = 25
+        nestedStackView.distribution = .FillEqually
+        nestedStackView.addArrangedSubview(UIButton(type: .InfoDark))
+        nestedStackView.addArrangedSubview(UIButton(type: .InfoLight))
+        nestedStackView.addArrangedSubview(UIButton(type: .ContactAdd))
+        stackView.addArrangedSubview(nestedStackView)
         
         let btn=UIButton(type: .System)
+        btn.setContentHuggingPriority(1000, forAxis: UILayoutConstraintAxis.Vertical)
         btn.setTitle("Press Me", forState: .Normal)
         btn.addTarget(self, action: "pressedme:", forControlEvents: UIControlEvents.TouchUpInside)
         stackView.addArrangedSubview(btn)
@@ -53,7 +63,7 @@ class ViewController: UIViewController {
     
     func pressedme(sender: UIButton!){
         UIView.animateWithDuration(0.5) {
-            self.lastLabel.hidden = !self.lastLabel.hidden
+            self.nestedStackView.hidden = !self.nestedStackView.hidden
         }
         //stackView.removeArrangedSubview(lastLabel)
         //lastLabel.removeFromSuperview()
